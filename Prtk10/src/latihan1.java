71c52ee5c99397693a54424dd75127c4864abaed
ElvinMustianto <mustianto298@gmail.com>
2023-05-23 19:22:35 +0700

    fix praktikum 10

--- Prtk10/src/latihan1.java
+++ Prtk10/src/latihan1.java
@@ -1,32 +1,23 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import static com.sun.deploy.uitoolkit.ToolkitStore.dispose;
 
 public class latihan1 {
     private JButton btnLatihan;
     private JPanel jPanel;
 
     public latihan1() {
-        btnLatihan.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                latihan2 next = new latihan2();
-                JFrame jFrame = new JFrame("Latihan 2");
+        btnLatihan.addActionListener(e -> {
+            latihan2 next;
+            next = new latihan2();
+            JFrame jFrame = new JFrame("Latihan 2");
 
-                jFrame.add(next.getjPanel());
-                jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                jFrame.pack();
-                jFrame.setVisible(true);
-                jFrame.setLocationRelativeTo(null);
-                jFrame.setSize(400,200);
-                try {
-                    dispose();
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-            }
+            jFrame.add(next.getjPanel());
+            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jFrame.pack();
+            jFrame.setVisible(true);
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setSize(400,200);
+
         });
     }
 
